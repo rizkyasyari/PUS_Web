c17b87c3e1158875a3fc795a87a3b703f0052ecc
Jihad Benastey <jbenastey@gmail.com>
2021-04-21 22:04:14 +0700

    fix pembayaran

--- routes/api.php
+++ routes/api.php
@@ -28,3 +28,5 @@ Route::get('users','ApiController@ambil_users');
 
 Route::post('login','Api\AuthApiController@login');
 Route::post('get-anak','Api\OrangtuaApiController@getAnak');
+Route::post('get-tagihan','Api\TagihanApiController@getTagihan');
+Route::post('update-tagihan','Api\TagihanApiController@updateTagihan');
