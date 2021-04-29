59026944a39930bc5fd5423d8cd457f9a513bbf4
RizkyAsyari <rizkyasyari7@gmail.com>
2021-04-29 17:02:59 +0700

    Fix tampilan saldo dan rekening

--- resources/views/saldo.blade.php
+++ resources/views/saldo.blade.php
@@ -19,18 +19,14 @@
                                 <img class="profile-pic" src="{{ url('img/users/admin.png')}}" style="width: 50%" alt="profile">
                                 <span class="profile-name">{{Auth::user()->name}}</span>
                             </div>
-                            <div class="d-none d-md-block">
-                                <button class="btn btn-primary btn-icon-text btn-edit-profile">
-                                    <i data-feather="edit" class="btn-icon-prepend"></i> Edit profile
-                                </button>
-                            </div>
+
                         </div>
                     </div>
                     <div class="header-links">
                         <ul class="links d-flex align-items-center mt-3 mt-md-0">
                             <li class="header-link-item d-flex align-items-center active">
-                                <i class="mr-1 icon-md" data-feather="columns"></i>
-                                <a class="pt-1px d-none d-md-block" href="#">Timeline</a>
+{{--                                <i class="mr-1 icon-md" data-feather="columns"></i>--}}
+{{--                                <a class="pt-1px d-none d-md-block" href="#">Timeline</a>--}}
                             </li>
 
                         </ul>
@@ -110,23 +106,75 @@
                             <div class="card-header">
                                 <div class="d-flex align-items-center justify-content-between">
                                         <h6 class="card-title mb-0">Saldo</h6>
+                                    <div class="d-none d-md-block">
+                                        <button class="btn btn-success btn-icon-text btn-edit-profile "data-toggle="modal" data-target="#exampleModal">
+                                            <i data-feather="edit" class="btn-icon-prepend"></i> Edit Rekening
+                                        </button>
+                                    </div>
+                                </div>
+                            </div>
 
+                            <div class="card-body">
+                                <div>
+                                    <label class="tx-11 font-weight-bold mb-0 text-uppercase">Nama Rekening:</label>
+                                    <p class="text-muted">{{Auth::user()->nama_rekening}}</p>
                                 </div>
                             </div>
                             <div class="card-body">
+                                <div>
+                                    <label class="tx-11 font-weight-bold mb-0 text-uppercase">Bank:</label>
+                                    <p class="text-muted">{{Auth::user()->bank_rekening}}</p>
+                                </div>
+                            </div>
+                            <div class="card-body">
+                                <div>
+                                    <label class="tx-11 font-weight-bold mb-0 text-uppercase">Nomor Rekening:</label>
+                                    <p class="text-muted">{{Auth::user()->nomor_rekening}}</p>
+                                </div>
+                            </div>
+
+                            <div class="card-body">
                                 <h4 class="mb-3 mb-md-0">Saldo Anda</h4>
                                 <h4><span class="badge badge-success" style="margin-top: 20px">Rp.100.000.000</span></h4>
-                                <button type="button" class="btn btn-primary btn-icon-text float-right" style="margin-top: 20px" data-toggle="modal" data-target="#exampleModal">
+                                <button type="button" class="btn btn-primary btn-icon-text float-right" style="margin-top: 20px" data-toggle="modal" data-target="#tarikModal">
                                     <i class="btn-icon-prepend" data-feather="credit-card"></i>
                                     Tarik
                                 </button>
+
+                                <!-- Modal -->
+                                <div class="modal fade" id="tarikModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                    <div class="modal-dialog" role="document">
+                                        <div class="modal-content">
+                                            <div class="modal-header">
+                                                <h5 class="modal-title" id="exampleModalLabel">Tarik</h5>
+                                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                                    <span aria-hidden="true">&times;</span>
+                                                </button>
+                                            </div>
+                                            <div class="modal-body">
+                                                <form class="forms-sample">
+                                                    <div class="form-group">
+                                                        <label for="exampleInputEmail1">Jumlah Penariakan</label>
+                                                        <input type="number" class="form-control" id="exampleInputEmail1" placeholder="Jumlah Penarikan">
+                                                    </div>
+                                                </form>
+                                            </div>
+                                            <p class="mb-3"h><code>*Proses penarikan akan membutuhkan tiga sampai tujuh hari kerja</code></p>
+
+                                            <div class="modal-footer">
+                                                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                                                <button type="button" class="btn btn-primary">Submit</button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
 
                                 <!-- Modal -->
                                 <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
                                     <div class="modal-dialog" role="document">
                                         <div class="modal-content">
                                             <div class="modal-header">
-                                                <h5 class="modal-title" id="exampleModalLabel">Transfer Bank</h5>
+                                                <h5 class="modal-title" id="exampleModalLabel">Edit Bank</h5>
                                                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                                                     <span aria-hidden="true">&times;</span>
                                                 </button>
@@ -142,34 +190,29 @@
                                                         <input type="number" class="form-control" id="exampleInputUsername1" autocomplete="off" placeholder="Nomor Rekening">
                                                     </div>
                                                     <div class="form-group">
-                                                        <label>Bank</label>
-                                                        <select class="js-example-basic-single w-auto" style="width: 450px">
-                                                            <option value="NY">BCA</option>
-                                                            <option value="FL">BRI</option>
-                                                            <option value="FL">BNI</option>
-                                                            <option value="FL">Mandiri</option>
-                                                        </select>
+                                                        <label for="exampleInputUsername1">Nama Bank</label>
+                                                        <input type="text" class="form-control" id="exampleInputUsername1" autocomplete="off" placeholder="Nama Bank">
                                                     </div>
                                                     <div class="form-group">
                                                         <label for="exampleInputEmail1">Jumlah Nominal</label>
                                                         <input type="number" class="form-control" id="exampleInputEmail1" placeholder="Jumlah Nominal">
                                                     </div>
-                                                    <div class="form-group">
-                                                        <label for="exampleInputPassword1">{{ __('Password') }}</label>
-                                                        <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-                                                        @error('password')
-                                                        <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                                        @enderror
+{{--                                                    <div class="form-group">--}}
+{{--                                                        <label for="exampleInputPassword1">{{ __('Password') }}</label>--}}
+{{--                                                        <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">--}}
+{{--                                                        @error('password')--}}
+{{--                                                        <span class="invalid-feedback" role="alert">--}}
+{{--                                        <strong>{{ $message }}</strong>--}}
+{{--                                    </span>--}}
+{{--                                                        @enderror--}}
 
-                                                    </div>
+{{--                                                    </div>--}}
                                                 </form>
                                             </div>
 
                                             <div class="modal-footer">
                                                 <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                                                <button type="button" class="btn btn-primary">Collect Cashout</button>
+                                                <button type="button" class="btn btn-primary">Submit</button>
                                             </div>
                                         </div>
                                     </div>
